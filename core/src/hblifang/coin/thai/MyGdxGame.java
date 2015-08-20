60f6be1a9fafc801d034d6dd4bbd5e2f7854e03f
hbsosweet <pong26032@hotmail.com>
2015-08-20 16:48:27 +0700

    Finish Day4

--- core/src/hblifang/coin/thai/MyGdxGame.java
+++ core/src/hblifang/coin/thai/MyGdxGame.java
@@ -2,21 +2,27 @@ package hblifang.coin.thai;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 
 public class MyGdxGame extends ApplicationAdapter {
 	//Explicit
 	private SpriteBatch batch;
-	private Texture wallpaperTexture, cloudTexture;
+	private Texture wallpaperTexture, cloudTexture, pigTexture;
 	private OrthographicCamera objOrthographicCamera;
 	private BitmapFont nameBitmapFont;
 	int xCloudAnInt, yCloudAnInt = 600;
 	private boolean cloudABoolean = true;
+	private Rectangle picRectangle;
+	private Vector3 objVector3;
+	private Sound pigSound;
 
 	@Override
 	public void create () {
@@ -36,6 +42,19 @@ public class MyGdxGame extends ApplicationAdapter {
 
 		//Setup Cloud
 		cloudTexture = new Texture("cloud.png");
+
+		//Setup Pig
+		pigTexture = new Texture("lk.png");
+
+		//Setup Rectangle pig
+		picRectangle = new Rectangle();
+		picRectangle.x = 560;
+		picRectangle.y = 100;
+		picRectangle.width = 80;
+		picRectangle.height = 56;
+
+		//Setup Pig Sound
+		pigSound = Gdx.audio.newSound(Gdx.files.internal("dog.wav"));
 
 	} //create เอาไว้กำหนดค่า
 
@@ -60,13 +79,43 @@ public class MyGdxGame extends ApplicationAdapter {
 		//Drawable BitmapFont
 		nameBitmapFont.draw(batch, "Little K Coins Collector", 50, 750);
 
+		//Drawable Pig
+		batch.draw(pigTexture, picRectangle.x, picRectangle.y);
+
 		batch.end();
 
 		//Move Cloud
 		moveCloud();
+		
+		//Active When Touch Screen
+		activeTouchScreen();
 
 
 	} //render ตัวนี้คือ loop
+
+	private void activeTouchScreen() {
+		if (Gdx.input.isTouched()) {
+
+			//Sound Effect Pig
+			pigSound.play();
+
+			objVector3 = new Vector3(); //ตัวทำหน้าที่ในการเก็บค่าเมื่อนิ้วไปโดน
+			objVector3.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			if (objVector3.x < 600) {
+				if (picRectangle.x < 0) {
+					picRectangle.x = 0;
+				} else {
+					picRectangle.x -= 10;
+				}
+			} else {
+				if (picRectangle.x > 1100) {
+					picRectangle.x = 1100;
+				} else {
+					picRectangle.x += 10;
+				}
+			}
+		} //if
+	}
 
 	private void moveCloud() {
 		if (cloudABoolean) {
