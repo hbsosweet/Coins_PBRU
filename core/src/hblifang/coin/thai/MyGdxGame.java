734d0d5a1701ed5da4ce225683e984749303f4bb
hbsosweet <pong26032@hotmail.com>
2015-08-20 15:27:11 +0700

    Move Cloud

--- core/src/hblifang/coin/thai/MyGdxGame.java
+++ core/src/hblifang/coin/thai/MyGdxGame.java
@@ -12,10 +12,12 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class MyGdxGame extends ApplicationAdapter {
 	//Explicit
 	private SpriteBatch batch;
-	private Texture wallpaperTexture;
+	private Texture wallpaperTexture, cloudTexture;
 	private OrthographicCamera objOrthographicCamera;
 	private BitmapFont nameBitmapFont;
-	
+	int xCloudAnInt, yCloudAnInt = 600;
+	private boolean cloudABoolean = true;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -31,6 +33,9 @@ public class MyGdxGame extends ApplicationAdapter {
 		nameBitmapFont = new BitmapFont();
 		nameBitmapFont.setColor(Color.YELLOW); //ใส่สี
 		nameBitmapFont.setScale(4); //scale font
+
+		//Setup Cloud
+		cloudTexture = new Texture("cloud.png");
 
 	} //create เอาไว้กำหนดค่า
 
@@ -49,9 +54,33 @@ public class MyGdxGame extends ApplicationAdapter {
 		//Draw Wallpaper
 		batch.draw(wallpaperTexture, 0, 0);
 
+		//Drawable Cloud
+		batch.draw(cloudTexture, xCloudAnInt, yCloudAnInt);
+
 		//Drawable BitmapFont
 		nameBitmapFont.draw(batch, "Little K Coins Collector", 50, 750);
 
 		batch.end();
+
+		//Move Cloud
+		moveCloud();
+
+
 	} //render ตัวนี้คือ loop
+
+	private void moveCloud() {
+		if (cloudABoolean) {
+			if (xCloudAnInt < 937) {
+				xCloudAnInt += 100 * Gdx.graphics.getDeltaTime();
+			} else {
+				cloudABoolean = !cloudABoolean;
+			}
+		} else {
+			if (xCloudAnInt > 0) {
+				xCloudAnInt -= 100 * Gdx.graphics.getDeltaTime();
+			} else {
+				cloudABoolean = !cloudABoolean;
+			}
+		}
+	}
 } //Main Class
