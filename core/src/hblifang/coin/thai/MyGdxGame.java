279100ce1f94f1ef24d0b6e04ac55c9294ba5d6a
hbsosweet <pong26032@hotmail.com>
2015-08-20 15:06:19 +0700

    Before Break Noon

--- core/src/hblifang/coin/thai/MyGdxGame.java
+++ core/src/hblifang/coin/thai/MyGdxGame.java
@@ -2,26 +2,56 @@ package hblifang.coin.thai;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	//Explicit
+	private SpriteBatch batch;
+	private Texture wallpaperTexture;
+	private OrthographicCamera objOrthographicCamera;
+	private BitmapFont nameBitmapFont;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+
+		//คือการกำหนดขนาดของจอที่ต้องการ
+		objOrthographicCamera = new OrthographicCamera();
+		objOrthographicCamera.setToOrtho(false, 1200, 800);
+
+		//Setup Wallpaper
+		wallpaperTexture = new Texture("wallpapers_b_05.png");
+
+		//Setup BitmapFont
+		nameBitmapFont = new BitmapFont();
+		nameBitmapFont.setColor(Color.YELLOW); //ใส่สี
+		nameBitmapFont.setScale(4); //scale font
+
+	} //create เอาไว้กำหนดค่า
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		//Setup Screen
+		objOrthographicCamera.update();
+		batch.setProjectionMatrix(objOrthographicCamera.combined);
+
+		//เอาไว้วาด Object
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+		//Draw Wallpaper
+		batch.draw(wallpaperTexture, 0, 0);
+
+		//Drawable BitmapFont
+		nameBitmapFont.draw(batch, "Little K Coins Collector", 50, 750);
+
 		batch.end();
-	}
-}
+	} //render ตัวนี้คือ loop
+} //Main Class
